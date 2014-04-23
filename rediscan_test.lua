4fb252b5f7a8980c6c3a4aa0b976ae26dbbcdee5
Michel Martens <michel@soveran.com>
2014-04-23 10:13:44 -0300

    Update to use resp 1.2.0

--- rediscan_test.lua
+++ rediscan_test.lua
@@ -2,7 +2,7 @@ require("pl.strict")
 
 local rediscan = require("rediscan")
 
-local resp = require("resp-6e21869")
+local resp = require("lib.resp-8ea9690")
 
 local c = resp.new("localhost", 6379)
 
